9e07adf7eebb00065e93a911a347cb483e3de14c
J.P <jpdenoyer@gmail.com>
2019-10-02 00:22:11 -0400

    JP 100219 timer.c

---
+++ app/src/main/cpp/global_constants.h
@@ -0,0 +1,21 @@
+#ifndef NULL
+#define    NULL    ('\0')
+#endif
+
+#define    MIN(a,b)    ((a)<(b)?(a):(b))
+#define    path "/home/pi/Desktop/TiniLite_Code/Raspberry_Pi/Src"
+
+
+#ifndef TRUE
+#define TRUE (1)
+#endif
+
+#ifndef FALSE
+#define FALSE (0)
+#endif
+
+#ifndef bool
+#define bool unsigned char
+#endif
+
+
---
+++ app/src/main/cpp/global_includes.h
@@ -0,0 +1,14 @@
+#ifndef global_includes_h
+#define global_includes_h
+
+//#define rasp_pi_linux
+#define android_native
+
+#include "helloneon-intrinsics.h"
+
+
+#include "timer.h"
+
+#include "global_constants.h"
+
+#endif
